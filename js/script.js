70fc47c8038c39d2864c81095d516e986e1baaca
Anna Pratnicka <pratnicka.ann@gmail.com>
2022-10-06 18:41:22 +0200

    Remove active article

--- js/script.js
+++ js/script.js
@@ -1,24 +1,29 @@
 'use strict';
 
-function titleClickHandler(event){
+function titleClickHandler(event) {
     console.log('Link was clicked!');
     console.log(event);
+    /* remove class 'active' from all article links  */
+    const activeLinks = document.querySelectorAll('.titles a.active');
+    for (let activeLink of activeLinks) {
+        activeLink.classList.remove('active');
+    }
+    /* add class 'active' to the clicked link */
+    const activeArticles = document.querySelectorAll('.posts article.active');
+    for (let activeArticle of activeArticles) {
+        activeArticle.classList.remove('active');
+    }
+    /* remove class 'active' from all articles */
 
-  /* remove class 'active' from all article links  */
+    /* get 'href' attribute from the clicked link */
 
-  /* add class 'active' to the clicked link */
+    /* find the correct article using the selector (value of 'href' attribute) */
 
-  /* remove class 'active' from all articles */
-
-  /* get 'href' attribute from the clicked link */
-
-  /* find the correct article using the selector (value of 'href' attribute) */
-
-  /* add class 'active' to the correct article */
+    /* add class 'active' to the correct article */
 }
-  
+
 const links = document.querySelectorAll('.titles a');
 
-for(let link of links){
+for (let link of links) {
     link.addEventListener('click', titleClickHandler);
 }
